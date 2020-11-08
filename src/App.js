3999fe981e97ee109f350b5d863b9375a8844824
Boris Pogrebnyak <52397814+BorisPogrebnyak@users.noreply.github.com>
2020-11-08 17:44:36 +0200

    del 111

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { CurrentDate } from "./components/CurrentDate";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox11111</h1>
+      <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <CurrentDate className="App-current-date" />
     </div>
