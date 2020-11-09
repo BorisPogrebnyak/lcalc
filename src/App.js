699c620d81cbcbebbf78201845ba7764c2c202e9
BorisPogrebnyak <borisu.pogrebnyaku@gmail.com>
2020-11-09 07:58:50 +0200

    add CyrrentTime

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 
 import { CurrentDate } from "./components/CurrentDate";
+import { CurrentTime } from "./components/CurrentTime";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <CurrentDate className="App-current-date" />
+      <CurrentTime className="App-current-time" />
     </div>
   );
 }
