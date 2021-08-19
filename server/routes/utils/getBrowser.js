9eb1c226aeb16120e57b4cf1f7d454f3ed201023
b999 <borisu.pv@gmail.com>
2021-08-19 17:18:56 +0300

    getBrowser puppeteer.launch headless: false --headless

--- server/routes/utils/getBrowser.js
+++ server/routes/utils/getBrowser.js
@@ -4,8 +4,8 @@ const puppeteer = require(`puppeteer`);
 async function getBrowser() {
   try {
     return await puppeteer.launch({
-      args: ['--no-sandbox'],
-      // headless: false, // Heroku - не работает
+      args: ['--no-sandbox', '--headless'],
+      headless: false, // Heroku - не работает
     });
   } catch (err) {
     console.error(`Не удалось создать экземпляр браузера => : ${err}`);
